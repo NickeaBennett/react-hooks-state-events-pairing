16f20a09d112a14c7f500f69653fd504d363e404
NickeaBennett <nickeabennett@gmail.com>
2022-06-19 08:21:24 -0400

    assignment

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,21 @@
+import Comment from './Comment'
+import { useState } from 'react'
+
+function CommentsContainer({ comments }) {
+    const [search, setSearch] = useState('')
+    const findUser = e => setSearch(e.target.value)
+    const foundUsers = comments.filter(commentObj => {
+        return commentObj.user.includes(search)
+    })
+
+    return (
+        <div>
+            <h2>{foundUsers.length} Comments</h2>
+            Search: <input placeholder="potato" onChange={findUser}/>
+
+            { foundUsers.map(commentObj => 
+                <Comment key={commentObj.id} comment={commentObj} />) }
+        </div>
+    )
+}
+export default CommentsContainer
